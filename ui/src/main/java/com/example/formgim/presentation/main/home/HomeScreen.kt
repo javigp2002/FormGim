391af724cf43e4a7f1f7309385761fafe2096ebf
Javier G Peregrin <javigp2002@gmail.com>
2025-04-23 18:02:52 +0200

    :sparkles: Add: Home composable as scaffold

--- ui/src/main/java/com/example/formgim/presentation/main/home/HomeScreen.kt
+++ ui/src/main/java/com/example/formgim/presentation/main/home/HomeScreen.kt
@@ -1,35 +1,45 @@
 package com.example.formgim.presentation.main.home
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(goToDetail: () -> Unit = {}) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column {
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            Text(
+                text = "Home",
+                modifier = Modifier.padding(16.dp),
+                style = androidx.compose.material3.MaterialTheme.typography.titleLarge
             )
-            TextButton(goToDetail) {
-                Text("Ir")
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(0.dp, innerPadding.calculateTopPadding(), 0.dp, 0.dp)
+        ) {
+            items(20) { index ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                ) {
+                    Text(
+                        text = "Card $index",
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
             }
         }
-
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
