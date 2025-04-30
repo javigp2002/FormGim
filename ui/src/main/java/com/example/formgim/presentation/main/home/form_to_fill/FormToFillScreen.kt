ba8505e263d123f957a5d8822662c7d552336425
Javier G Peregrin <javigp2002@gmail.com>
2025-04-30 19:34:06 +0200

    :sparkles: Update: Use hiltViewModel in FormToFillScreen and fetch questions on launch

--- ui/src/main/java/com/example/formgim/presentation/main/home/form_to_fill/FormToFillScreen.kt
+++ ui/src/main/java/com/example/formgim/presentation/main/home/form_to_fill/FormToFillScreen.kt
@@ -5,13 +5,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun FormToFillScreen(viewModel: FormToFillVm = FormToFillVm()) {
+fun FormToFillScreen(viewModel: FormToFillVm = hiltViewModel()) {
     val listOfQuestions by viewModel.listOfQuestions.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.getListOfQuestionsFromFormId(1)
+    }
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Greeting(
