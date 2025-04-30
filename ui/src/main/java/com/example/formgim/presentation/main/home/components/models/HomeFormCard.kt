629ed81295faffbed4216be428b2165ee759d480
Javier G Peregrin <javigp2002@gmail.com>
2025-04-30 17:23:18 +0200

    :sparkles: Delete: Models are in domain layer

--- ui/src/main/java/com/example/formgim/presentation/main/home/components/models/HomeFormCard.kt
+++
@@ -1,8 +0,0 @@
-package com.example.formgim.presentation.main.home.components.models
-
-data class HomeFormCard(
-    var id: Int,
-    var title: String,
-    var author: String,
-    var onClick: () -> Unit
-)
