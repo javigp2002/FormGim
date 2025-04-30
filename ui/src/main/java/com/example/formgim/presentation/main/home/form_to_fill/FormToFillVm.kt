cc69fccb97caa274c2e74308147051b0609a809e
Javier G Peregrin <javigp2002@gmail.com>
2025-04-30 20:02:44 +0200

    :sparkles: Fix: Decorator and Refactor FormToFillVm to use viewModelScope for coroutine support in getListOfQuestionsFromFormId

--- ui/src/main/java/com/example/formgim/presentation/main/home/form_to_fill/FormToFillVm.kt
+++ ui/src/main/java/com/example/formgim/presentation/main/home/form_to_fill/FormToFillVm.kt
@@ -1,13 +1,17 @@
 package com.example.formgim.presentation.main.home.form_to_fill
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.appgim.domain.main.home.models.form.QuestionTypes
 import com.appgim.domain.main.home.usecases.GetListOfQuestionsFromForm
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class FormToFillVm @Inject constructor(
     private val getListOfQuestionsFromForm: GetListOfQuestionsFromForm
 ) : ViewModel() {
@@ -16,8 +20,11 @@ class FormToFillVm @Inject constructor(
 
 
     fun getListOfQuestionsFromFormId(id: Int) {
-        getListOfQuestionsFromForm.run(id).let { questions ->
-            _listOfQuestions.value = questions
+        viewModelScope.launch {
+            getListOfQuestionsFromForm.run(id).let { questions ->
+                _listOfQuestions.value = questions
+            }
         }
     }
+
 }
