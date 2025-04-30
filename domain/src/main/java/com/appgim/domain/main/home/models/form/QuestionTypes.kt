af8286edb71c15f430d0148f7fb4193007736784
Javier G Peregrin <javigp2002@gmail.com>
2025-04-30 19:32:47 +0200

    :sparkles: Add: QuestionTypes sealed class and update ListFormState to include loading and error states

--- domain/src/main/java/com/appgim/domain/main/home/models/form/QuestionTypes.kt
+++ domain/src/main/java/com/appgim/domain/main/home/models/form/QuestionTypes.kt
@@ -1,9 +1,4 @@
-package com.example.formgim.presentation.main.home.form_to_fill.states
-
-import com.appgim.domain.main.home.models.form.MultipleOptionModel
-import com.appgim.domain.main.home.models.form.SingleOptionModel
-import com.appgim.domain.main.home.models.form.SliderBoxModel
-import com.appgim.domain.main.home.models.form.TextBoxModel
+package com.appgim.domain.main.home.models.form
 
 sealed class QuestionTypes(
     val id: Int,
@@ -12,8 +7,10 @@ sealed class QuestionTypes(
     class TextBox(textBoxModel: TextBoxModel) : QuestionTypes(textBoxModel.id, textBoxModel.title)
     class Slider(sliderBoxModel: SliderBoxModel) :
         QuestionTypes(sliderBoxModel.id, sliderBoxModel.question)
+
     class Multiple(multipleOptionModel: MultipleOptionModel) :
         QuestionTypes(multipleOptionModel.id, multipleOptionModel.question)
+
     class SingleOption(singleOptionModel: SingleOptionModel) :
         QuestionTypes(singleOptionModel.id, singleOptionModel.question)
 }
--- ui/src/main/java/com/example/formgim/presentation/main/home/form_to_fill/states/ListState.kt
+++ ui/src/main/java/com/example/formgim/presentation/main/home/form_to_fill/states/ListState.kt
@@ -1,5 +1,7 @@
 package com.example.formgim.presentation.main.home.form_to_fill.states
 
+import com.appgim.domain.main.home.models.form.QuestionTypes
+
 data class ListFormState(
     var isLoading: Boolean = false,
     var error: String = "",
