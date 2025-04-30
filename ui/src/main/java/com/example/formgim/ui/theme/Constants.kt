cbe952c65b513d9f1afaa10b1bc445e0d742de98
Javier G Peregrin <javigp2002@gmail.com>
2025-04-30 11:54:25 +0200

    :sparkles: Add: Constants for padding sizes and dimensions

---
+++ ui/src/main/java/com/example/formgim/ui/theme/Constants.kt
@@ -0,0 +1,16 @@
+package com.example.formgim.ui.theme
+
+object Constants {
+    object PaddingSizes {
+        const val S = 4
+        const val M = 8
+        const val L = 16
+        const val XL = 20
+        const val XXL = 32
+    }
+
+    object Dimensions {
+        const val CARD_ELEVATION = 4
+        const val CARD_RADIUS = 8
+    }
+}
