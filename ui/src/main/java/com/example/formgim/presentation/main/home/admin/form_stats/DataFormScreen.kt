d6eaa036428ce974adb64f65557623bf39483ee1
Javier G Peregrin <javigp2002@gmail.com>
2025-06-02 12:20:24 +0200

    :rocket: Add: Implement DataFormScreen and ViewModel for form data management

---
+++ ui/src/main/java/com/example/formgim/presentation/main/home/admin/form_stats/DataFormScreen.kt
@@ -0,0 +1,63 @@
+package com.example.formgim.presentation.main.home.admin.form_stats
+
+import MyTopAppBar
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.formgim.ui.theme.Constants
+
+@Composable
+fun DataFormScreen(onBack: () -> Unit, viewModel: DataFormVM = hiltViewModel()) {
+    val listFormState by viewModel.stateOfView.collectAsState()
+
+    LaunchedEffect(Unit) {}
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = Constants.PaddingSizes.M.dp,
+                vertical = Constants.PaddingSizes.L.dp
+            ),
+        topBar = {
+            MyTopAppBar(
+                title = "Información del formulario",
+                backEvent = { onBack() },
+                actions = {
+                    IconButton(
+                        onClick = { }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Save,
+                            contentDescription = "Guardar Formulario"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        if (listFormState.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding.calculateTopPadding())
+            )
+        } else {
+            Box {}
+        }
+    }
+}
