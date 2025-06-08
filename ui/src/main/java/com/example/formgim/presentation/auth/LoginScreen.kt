e4118c397a3da49787da30543bf78ae0ef06f049
Javier G Peregrin <javigp2002@gmail.com>
2025-06-08 02:08:43 +0200

    :art: Simplify LoginScreen layout by replacing TextButton with Button for improved user experience

--- ui/src/main/java/com/example/formgim/presentation/auth/LoginScreen.kt
+++ ui/src/main/java/com/example/formgim/presentation/auth/LoginScreen.kt
@@ -1,27 +1,21 @@
 package com.example.formgim.presentation.auth
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -62,21 +56,6 @@ fun LoginScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            TextButton(
-                onClick = {
-                    onSuccessGoToMain()
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(1.dp, Color.Black)
-            ) {
-                Text("Iniciar sesión")
-            }
-
-            Spacer(modifier = Modifier.height(32.dp))
 
             Button(
                 onClick = {
